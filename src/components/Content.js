7600b4ad4ee483bf83c24b4e1da089f78860827b
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-07-05 13:04:05 +0200

    Add files via upload

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,50 +2,58 @@ import React, {useState} from 'react';
 
 function Content(props) {
     // const [Content, setContent] =  useState(props.content)
+    const [showHide, setShowHide] = useState(false)
     const [step, setStep] = useState(1)
+    console.log(props.content.length)
 
-    // const answer = (id) => {
+
+    // const stepControl1 = () => {
     //     setStep(2)
-        // const newContent = [...Content].map(element =>
-        //     element.id === id ? element : null
-        // )
-        // setContent(newContent)
+    //     if (x < props.content.length) {
+    //         x = x + 1
+    //     }
     // }
-    let x = 0
-    console.log(Content)
-    // const quizStart = props.content.map(element => 
-    //     <div key={element[x].id}>
-    //         <div>{element[x][0]}</div>          
-    //     </div>
-    //  )
+    let x = 0;
+    // const stepControl = () => {
+    //     if (x < props.content.length) {
+    //         x = x + 1
+    //         setStep(1)
 
-    switch(step) {
-        case 1:
+    //     }
+    // }
+
+    const hideButton = {
+        display: "none"
+    }
+
+    const showButton = {
+        display: "block"
+    }
+
+    // for (x = x ; x < props.content.length; x++) {
+        switch(step) {
+            case 1:
+            console.log(x)
             return (
                 <div>
-                    <div>{props.name}</div>
-                    <div>Question</div>
-                        <div key={props.content[x].id}>
-                        <div>{props.content[x][0]}</div>
-                        <input type="button" value="Answer" onClick={() => setStep(2)} />
-                    </div>        
-                </div>
-             )
-
-        case 2: 
-        return (
-            <div>
                 <div>{props.name}</div>
-                <div>Question</div>
                     <div key={props.content[x].id}>
-                    <div>{props.content[x][0]}</div>
-                    <div>{props.content[x][1]}</div>                    
-                </div>        
-            </div>
-         )
-         default: (console.log("This is build by switch statement"))
-
-    }
+                        <div>Question</div>
+                        <div>{props.content[x][0]}</div>
+                        <input style={showHide ? hideButton : null} type="button" value="Answer" onClick={() => setShowHide(!showHide)} />
+                        
+                        <div style={showHide ? showButton : {display: "none"}}>
+                        <div>Answer</div>
+                        <div>{props.content[x][1]}</div>                 
+                        <input type="button" value="Next" onClick={() => setStep(2)}/>
+                        </div>
+                    </div>
+                </div>
+                 )
+ 
+             default: (console.log("This is build by switch statement"))
+        }  
+    // }
 
 }
 
