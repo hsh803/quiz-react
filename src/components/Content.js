49592ffc8ade1cb0d4a70db7ef986f248785a9ac
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-07-05 17:49:05 +0200

    Add files via upload

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,26 +1,32 @@
 import React, {useState} from 'react';
 
 function Content(props) {
-    // const [Content, setContent] =  useState(props.content)
     const [showHide, setShowHide] = useState(false)
     const [step, setStep] = useState(1)
+    const [count, setCount] = useState(0)
+    const [point, setPoint] = useState(0)
     console.log(props.content.length)
 
+    const winStep = () => {
+        if (count < props.content.length - 1) {
+            setCount(count + 1)
+            setPoint(point + 1)
+            setShowHide(!showHide)
+        }
+        else {
+            setPoint(point + 1)
+            setStep(2)}
 
-    // const stepControl1 = () => {
-    //     setStep(2)
-    //     if (x < props.content.length) {
-    //         x = x + 1
-    //     }
-    // }
-    let x = 0;
-    // const stepControl = () => {
-    //     if (x < props.content.length) {
-    //         x = x + 1
-    //         setStep(1)
+    }
 
-    //     }
-    // }
+    const loseStep = () => {
+        if (count < props.content.length) {
+            setCount(count + 1)
+            setShowHide(!showHide)
+        }
+        else {
+            setStep(2)}
+    }
 
     const hideButton = {
         display: "none"
@@ -29,31 +35,36 @@ function Content(props) {
     const showButton = {
         display: "block"
     }
+    switch(step) {
+    case 1:
+        return (
+            <div>
+            <div>{props.name}</div>
+            <div>{count} of {props.content.length}</div>
+            <div>{point}</div>
 
-    // for (x = x ; x < props.content.length; x++) {
-        switch(step) {
-            case 1:
-            console.log(x)
-            return (
-                <div>
-                <div>{props.name}</div>
-                    <div key={props.content[x].id}>
-                        <div>Question</div>
-                        <div>{props.content[x][0]}</div>
-                        <input style={showHide ? hideButton : null} type="button" value="Answer" onClick={() => setShowHide(!showHide)} />
-                        
-                        <div style={showHide ? showButton : {display: "none"}}>
-                        <div>Answer</div>
-                        <div>{props.content[x][1]}</div>                 
-                        <input type="button" value="Next" onClick={() => setStep(2)}/>
-                        </div>
+                <div key={props.content[count].id}>
+                    <div>Question</div>
+                    <div>{props.content[count][0]}</div>
+                    <input style={showHide ? hideButton : null} type="button" value="Answer" onClick={() => setShowHide(!showHide)} />
+                    
+                    <div style={showHide ? showButton : {display: "none"}}>
+                    <div>Answer</div>
+                    <div>{props.content[count][1]}</div>                 
+                    <input type="button" value="O" onClick={winStep}/>
+                    <input type="button" value="X" onClick={loseStep}/>
                     </div>
                 </div>
-                 )
- 
-             default: (console.log("This is build by switch statement"))
-        }  
-    // }
+            </div>
+            )
+
+    case 2:
+        return(
+            <div>You have got {point} points!</div>
+        )
+    
+    default: (console.log("This is build by switch statement"))
+    } 
 
 }
 
