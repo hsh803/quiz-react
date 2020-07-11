8dde02e0192177526eba6c676288599430cd5dd9
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-07-11 09:09:27 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
 import List from './components/List';
+import Content from './components/Content';
 
 
 function App() {
   const [Quiz, setQuiz] =  useState([])
+  const [QuizContent, setQuizContent] = useState()
 
   const quizList = (createQuiz) => {
     createQuiz.id = Math.random()
@@ -16,14 +18,24 @@ function App() {
     setQuiz(newQuiz)
   }
 
-
+  const quizContent = (id) => {
+    setQuizContent()
+    const content = [...Quiz].filter(element =>
+      element.id === id
+      )
+      console.log(content)
+    setQuizContent(content[0])
+  }
+  console.log(QuizContent)
+  console.log(Quiz)
 
   return (
     <BrowserRouter>
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/create" component={() => <Create submit={quizList} />} />
-      <Route exact path="/list" component={() => <List list={Quiz} />} />
+      <Route exact path="/list" component={() => <List list={Quiz} quiz={quizContent} />} />
+      <Route exact path="/list/quiz" component={() => <Content name={QuizContent.name} content={QuizContent.content} />} />
     </BrowserRouter>
   )
   
