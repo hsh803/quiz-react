05540a84802d50262abf45f241c0df7ebadff253
Hanna Han <hsh803@gmail.com>
2020-07-15 08:50:10 +0200

    Updates

---
+++ precache-manifest.0c817759ab95cdaffb5eac5e66dd56b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1f9eaf219485a72fca062992d9cf6a",
+    "url": "/quiz-react/index.html"
+  },
+  {
+    "revision": "ad40641eb3a31d38a14d",
+    "url": "/quiz-react/static/css/main.fd112ba6.chunk.css"
+  },
+  {
+    "revision": "e2d16e88fa04925ecd9a",
+    "url": "/quiz-react/static/js/2.bb98d8ed.chunk.js"
+  },
+  {
+    "revision": "c5736e10a027dc7eb2aacc7f7cc474aa",
+    "url": "/quiz-react/static/js/2.bb98d8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad40641eb3a31d38a14d",
+    "url": "/quiz-react/static/js/main.bb03298a.chunk.js"
+  },
+  {
+    "revision": "5831194fe6d422726258",
+    "url": "/quiz-react/static/js/runtime-main.7dc5cf93.js"
+  }
+]);
